e3fe1282919ba84d7143a89fb51511748699cf28
Davi Lacerda <dlacerda9@gmail.com>
2023-03-09 17:29:54 -0300

    feat(Jump): added jump skill

---
+++ strategy/src/Skills/Jump/Concrete/MidJump.java
@@ -0,0 +1,10 @@
+package Skills.Jump.Concrete;
+
+import Skills.Jump.Jump;
+
+public class MidJump implements Jump {
+    @Override
+    public void jump() {
+        System.out.println("Mid Jump");
+    }
+}
