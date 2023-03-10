11dc1c5b1a492cc07741a620e9d6211cb562c540
Davi Lacerda <dlacerda9@gmail.com>
2023-03-09 21:25:50 -0300

    refactor(strategy): remove unused classes

--- strategy/src/Skills/Attack/Concrete/HighAttack.java
+++
@@ -1,9 +0,0 @@
-package Skills.Attack.Concrete;
-import Skills.Attack.Attack;
-
-public class HighAttack implements Attack {
-    @Override
-    public void attack() {
-        System.out.println("High Attack");
-    }
-}
