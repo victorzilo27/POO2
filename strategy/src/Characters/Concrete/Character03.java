84f0c1b2192486f3fa7015706198de15731a5362
Davi Lacerda <dlacerda9@gmail.com>
2023-03-09 21:24:33 -0300

    refactor(strategy): change attack names

--- strategy/src/Characters/Concrete/Character03.java
+++ strategy/src/Characters/Concrete/Character03.java
@@ -1,13 +1,13 @@
 package Characters.Concrete;
 
 import Characters.Character;
-import Skills.Attack.Concrete.HighAttack;
+import Skills.Attack.Concrete.StrongAttack;
 import Skills.Jump.Concrete.LowJump;
 import Skills.Run.Concrete.FastRun;
 
 public class Character03 extends Character {
     public Character03(){
-        setAttack(new HighAttack());
+        setAttack(new StrongAttack());
         setJump(new LowJump());
         setRun(new FastRun());
     }
