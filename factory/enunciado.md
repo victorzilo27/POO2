be373c4d90741e9495f9cd6e1c52b27ef426ce8d
Davi Lacerda <dlacerda9@gmail.com>
2023-05-07 12:35:18 -0300

    feat(factory): implements SimpleGame

--- factory/enunciado.md
+++ factory/enunciado.md
@@ -4,7 +4,7 @@
 
 Aplique o padrão factory method, criando uma implementação abstrata da
 fábrica, mas deixando cada sub-classe concreta decidir quais personagens
-criar. 
+criar.
 
 Na classe abstrata crie um método jogar() que simula uma lógica de
 execução do game, e utiliza o factory method para fabricar personagens. O
--- factory/src/Factory/Concrete/SimpleGame.java
+++ factory/src/Factory/Concrete/SimpleGame.java
@@ -2,9 +2,21 @@ package Factory.Concrete;
 
 import Factory.Game;
 import Strategy.Character.Character;
+import Strategy.Character.Concrete.Character01;
+import Strategy.Character.Concrete.Character02;
+import Strategy.Character.Concrete.Character03;
 
 public class SimpleGame extends Game {
     public Character createCharacter(int type){
-        // TODO: Implement this method
+        switch(type){
+            case 1:
+                return new Character01();
+            case 2:
+                return new Character02();
+            case 3:
+                return new Character03();
+            default:
+                return null;
+        }
     }
 }
